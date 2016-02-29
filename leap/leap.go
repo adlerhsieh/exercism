e5a18250304b4882f3068c5ff7a9481f0666b865
Adler <nkj20932@hotmail.com>
2016-02-29 11:27:56 +0800

    complete leap year

--- leap/leap.go
+++ leap/leap.go
@@ -13,6 +13,14 @@ package leap
 const testVersion = 2
 
 // It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
+func IsLeapYear(year int) bool {
+	switch {
+	case year%400 == 0:
+		return true
+	case year%100 == 0:
+		return false
+	case year%4 == 0:
+		return true
+	}
+	return false
 }
