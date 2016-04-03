b8dc201fd28c89a73cfe3d75695f0712a3a239ea
Adler <nkj20932@hotmail.com>
2016-04-03 09:37:39 +0800

    solve raindrops

---
+++ raindrops/raindrops.go
@@ -0,0 +1,22 @@
+package raindrops
+
+import "fmt"
+
+const testVersion = 2
+
+func Convert(num int) string {
+	var result string
+	if num%3 == 0 {
+		result += "Pling"
+	}
+	if num%5 == 0 {
+		result += "Plang"
+	}
+	if num%7 == 0 {
+		result += "Plong"
+	}
+	if result == "" {
+		result = fmt.Sprintf("%d", num)
+	}
+	return result
+}
