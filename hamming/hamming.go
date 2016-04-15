41266d49cf110feb9c40ae5322bfa44bc3fab617
Adler <nkj20932@hotmail.com>
2016-04-15 22:28:42 +0800

    refactor hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,25 +1,18 @@
 package hamming
 
-import (
-	"errors"
-	"strings"
-)
+import "errors"
 
 const testVersion = 4
 
 // Distance measures the Hamming Distance between two strings.
-// Each mismatch character accounts for 1 point.
-// Measuring two strings with different length will return errors.
 func Distance(a, b string) (int, error) {
-	a_slice := strings.Split(a, "")
-	b_slice := strings.Split(b, "")
-	diff := 0
-	if len(a_slice) != len(b_slice) {
+	if len(a) != len(b) {
 		return -1, errors.New("Error")
 	}
-	for i, char := range a_slice {
-		if char != b_slice[i] {
-			diff += 1
+	diff := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			diff++
 		}
 	}
 	return diff, nil
